13e345ba9c21be400a1f7fc8a14739243aa7a9fd
Ilya Antipenko <ilya@antipenko.pp.ua>
2015-12-23 15:26:01 +0200

    Get only keys of tagged services & optimization
    - get only keys of tagged services
    - optimization

--- DependencyInjection/CompilerPass/ManagerCompilerPass.php
+++ DependencyInjection/CompilerPass/ManagerCompilerPass.php
@@ -8,27 +8,28 @@ use Symfony\Component\DependencyInjection\Reference;
 
 class ManagerCompilerPass implements CompilerPassInterface
 {
+    const LOADER_SERVICE = 'grossum.entity_manager_loader';
+    const CUSTOM_MANAGER_TAG = 'entity.custom_manager';
+    
     /**
      * @param ContainerBuilder $container
-     *
-     * @SuppressWarnings(PHPMD.UnusedLocalVariable)
      */
     public function process(ContainerBuilder $container)
     {
-        if (!$container->has('grossum.entity_manager_loader')) {
+        if (!$container->has(self::LOADER_SERVICE)) {
             return;
         }
 
-        $definition = $container->findDefinition(
-            'grossum.entity_manager_loader'
-        );
-
-        $taggedServices = $container->findTaggedServiceIds(
-            'entity.custom_manager'
-        );
-
-        foreach ($taggedServices as $id => $tags) {
-            $definition->addMethodCall(
+        $taggedServices = $container->findTaggedServiceIds(self::CUSTOM_MANAGER_TAG);
+        
+        if (empty($taggedServices)) {
+            return;
+        }
+        
+        $loaderDefinition = $container->findDefinition(self::LOADER_SERVICE);
+        
+        foreach (array_keys($taggedServices) as $id) {
+            $loaderDefinition->addMethodCall(
                 'addManager',
                 [new Reference($id)]
             );
